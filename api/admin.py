25cc8aa7e3f31c7cf4993ae4915a33c28d8b4104
Tanveer Najib <tanveernajib@gmail.com>
2022-04-25 14:35:13 +0100

    admin site bulk record locking

--- api/admin.py
+++ api/admin.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 from .models import Manga, Volume
 
 # Register your models here.
-admin.site.register(Manga)
+@admin.action(description='Mark selected as locked')
+def lock_records(modeladmin, request, queryset):
+    queryset.update(locked=True)
+
+@admin.action(description='Mark selected as unlocked')
+def unlock_records(modeladmin, request, queryset):
+    queryset.update(locked=False)
 
 @admin.register(Volume)
 class VolumeAdmin(admin.ModelAdmin):
-    list_filter = ('manga',) 
+    list_filter = ('manga',)
+    actions = (lock_records, unlock_records)
+
+@admin.register(Manga)
+class VolumeAdmin(admin.ModelAdmin):
+    actions = (lock_records, unlock_records)
