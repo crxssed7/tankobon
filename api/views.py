b8a4a81031feb81ce3f125ba69fcbce85e956ad9
Tanveer Najib <tanveernajib@gmail.com>
2022-07-29 17:20:35 +0100

    added limit and sorting in get_manga

--- api/views.py
+++ api/views.py
@@ -6,17 +6,28 @@ from django.db.models import Q
 # Create your views here.
 def get_manga(request):
     offset = request.GET.get('offset')
+    _limit = request.GET.get('limit')
     query = request.GET.get('q')
+    sort = request.GET.get('sort')
+    sort_field = 'name'
     try:
         offset = int(offset)
     except:
         offset = 0
+    if sort:
+        if sort.lower() == 'last_updated':
+            sort_field = 'last_updated'
     limit = 8
+    if _limit:
+        try:
+            limit = int(_limit)
+        except:
+            limit = 8
     manga = []
     if not query:
-        manga = list(Manga.objects.values().order_by('name')[offset:offset + limit])
+        manga = list(Manga.objects.values().order_by(sort_field)[offset:offset + limit])
     else:
-        manga = list(Manga.objects.filter(Q(name__icontains=query) | Q(romaji__icontains=query)).values().order_by('name')[offset:offset + limit])
+        manga = list(Manga.objects.filter(Q(name__icontains=query) | Q(romaji__icontains=query)).values().order_by(sort_field)[offset:offset + limit])
     data = {
         'manga': manga
     }
