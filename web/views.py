9f14116debdecbdb5f2c36d9dd5d6dc61ffbf25e
Tanveer Najib <tanveernajib@gmail.com>
2022-09-28 15:40:39 +0100

    fix orderby

--- web/views.py
+++ web/views.py
@@ -54,8 +54,8 @@ class SearchResultsView(ListView):
     def get_context_data(self,**kwargs):
         context = super(SearchResultsView, self).get_context_data(**kwargs)
         context['recently_updated'] = Manga.objects.all().order_by('-last_updated')[:8]
-        context['releasing'] = Manga.objects.filter(status="RELEASING")[:5]
-        context['completed'] = Manga.objects.filter(status="FINISHED")[:5]
+        context['releasing'] = Manga.objects.filter(status="RELEASING").order_by('-last_updated')[:5]
+        context['completed'] = Manga.objects.filter(status="FINISHED").order_by('-last_updated')[:5]
         context['search_active'] = 'active'
         q = self.request.GET.get("q")
         if q:
