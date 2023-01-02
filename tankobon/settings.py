4ff7fe65686c2c5cc024e81f105a04025546e703
Tanveer Najib <tanveernajib@gmail.com>
2023-01-02 15:24:07 +0000

    trying to fix deploy circleci

--- tankobon/settings.py
+++ tankobon/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from environs import Env
 
 env = Env()
-env.read_env(override=True)
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = env.str("SECRET_KEY", default="test")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
