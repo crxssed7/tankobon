8611303dda16901b34b2a6ff461397332629ca17
Tanveer Najib <tanveernajib@gmail.com>
2023-01-02 15:14:01 +0000

    trying to fix deploy circleci

--- tankobon/settings.py
+++ tankobon/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from environs import Env
 
 env = Env()
-env.read_env()
+env.read_env(override=True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
