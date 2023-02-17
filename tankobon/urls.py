58bf6224e12584025494ec35c4d1bd3ccba07c3a
Tanveer Najib <tanveernajib@gmail.com>
2023-02-17 17:28:27 +0000

    add robots

--- tankobon/urls.py
+++ tankobon/urls.py
@@ -17,12 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("web.urls")),
     path("api/", include("api.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots")
 ]
 
 if settings.DEBUG:
