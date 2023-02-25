270e5b3368d2e1af9222e9c8594fe9c4daa31a9e
Tanveer Najib <tanveernajib@gmail.com>
2023-02-25 09:16:31 +0000

    Move ALLOWED_HOSTS to env vars

--- tankobon/settings.py
+++ tankobon/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env.str("SECRET_KEY", default="test")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["tankobon.net"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", ["127.0.0.1"])
 CSRF_TRUSTED_ORIGINS = ["https://tankobon.net"]
 
 # Application definition
